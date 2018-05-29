dfa3c21347c55d5b7aa2910590b810d4f0b029b8
Pedro Victor <pedro.victor.ferreira@outlook.com.br>
2018-05-28 22:37:48 -0400

    Unity custom editor

---
+++ Assets/LSystem2Unity/Editor/RulesetEditor.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditorInternal;
+using LSystem2Unity;
+
+namespace LSystem2Unity.ToolEditor {
+	[CustomEditor(typeof(Ruleset))]
+	public class RulesetEditor : Editor {
+
+		private ReorderableList list;
+
+		/// <summary>
+		/// This function is called when the object becomes enabled and active.
+		/// </summary>
+		void OnEnable()
+		{
+			list = new ReorderableList(serializedObject, serializedObject.FindProperty("rules"),
+			false,true,true,true);
+
+			list.drawElementCallback = (Rect rect, int index, bool isActive, bool isFocused) => {
+				var element = list.serializedProperty.GetArrayElementAtIndex(index);
+				rect.y += 2;
+				EditorGUI.PropertyField(
+					new Rect(rect.x, rect.y, 30, EditorGUIUtility.singleLineHeight),element.FindPropertyRelative("symbol"), GUIContent.none);
+				EditorGUI.PropertyField(
+					new Rect(rect.x + 60, rect.y, rect.width - 60 - 30 , EditorGUIUtility.singleLineHeight), element.FindPropertyRelative("replace"), GUIContent.none);
+				
+			};
+
+			list.drawHeaderCallback = (Rect rect) => {
+				EditorGUI.LabelField(rect, "Rules (char -> string)");
+			};
+
+		}
+
+		public override void OnInspectorGUI(){
+			serializedObject.Update();
+			list.DoLayoutList();
+			serializedObject.ApplyModifiedProperties();
+		}
+
+	}
+}
---
+++ Assets/LSystem2Unity/Editor/TurtleRulesetEditor.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditorInternal;
+using LSystem2Unity;
+using UnityEngine.Events;
+using System;
+using System.Linq;
+
+
+namespace LSystem2Unity.ToolEditor{
+	[CustomEditor(typeof(TurtleAgent))]
+	public class TurtleRulesetEditor : Editor {
+		private ReorderableList list;
+		private TurtleAgent turtleRuleset;
+
+		private const string headerTemplate = "{0} {1}";
+		private const string callTurtleInitLabel = "Call event before Turtle process";
+
+		/// <summary>
+		/// This function is called when the object becomes enabled and active.
+		/// </summary>
+		void OnEnable()
+		{
+
+			turtleRuleset = (TurtleAgent)target;
+			list = CreateList(serializedObject, serializedObject.FindProperty("rules"));
+			
+		}
+
+		public override void OnInspectorGUI(){
+
+			EditorGUILayout.BeginHorizontal();
+			EditorGUILayout.LabelField(callTurtleInitLabel);
+			turtleRuleset.callTurtleInit = EditorGUILayout.Toggle(turtleRuleset.callTurtleInit);
+			EditorGUILayout.EndHorizontal();
+
+			if(turtleRuleset.callTurtleInit){
+				EditorGUILayout.BeginVertical();
+				var initEvent = serializedObject.FindProperty("startTurtle");
+				EditorGUILayout.PropertyField(initEvent);
+				EditorGUILayout.EndVertical();
+			}
+
+			serializedObject.Update();
+			list.DoLayoutList();
+			serializedObject.ApplyModifiedProperties();
+		}
+
+		private ReorderableList CreateList(SerializedObject obj, SerializedProperty prop){
+			ReorderableList list = new ReorderableList (obj, prop, false, true, true, true);
+
+			list.drawHeaderCallback = rect => {
+                        EditorGUI.LabelField (rect, "Turtle Rules");
+                };
+ 
+            List<float> heights = new List<float> (prop.arraySize);
+ 
+			list.drawElementCallback = (Rect rect, int index, bool isActive, bool isFocused) => {
+				var element = list.serializedProperty.GetArrayElementAtIndex(index);
+				SerializedProperty s = element.FindPropertyRelative("Symbol");
+				SerializedProperty c = element.FindPropertyRelative("Action");
+				var elementSymbol = turtleRuleset.rules[index].Symbol;
+
+				bool foldout = isActive;
+				
+				float height = EditorGUIUtility.singleLineHeight * 1.25f;
+				//if(foldout){height = EditorGUIUtility.singleLineHeight * 5;}
+
+				try{
+					heights[index] = height;
+				} catch(ArgumentOutOfRangeException e){
+					Debug.LogWarning(e.Message);
+				} finally{
+					float[] floats = heights.ToArray();
+					Array.Resize(ref floats, prop.arraySize);
+					heights = floats.ToList();
+				}
+				
+
+				float margin = height / 100;
+				//rect.y += margin;
+				rect.height = (height / 5) * 4;
+				rect.width = rect.width /2 - margin/2;
+
+				if(foldout){
+					if(s != null && c != null){
+						EditorGUILayout.PropertyField(s);
+						EditorGUILayout.PropertyField(c);
+					}
+				}
+				
+				
+				rect.x = 20;
+				EditorGUI.LabelField(rect, string.Format(headerTemplate, s.displayName, elementSymbol.ToString()));
+
+			};
+			
+			list.elementHeightCallback = (index) => {
+				Repaint ();
+				float height = 0;
+
+				try {
+					height = heights [index];
+				} catch (ArgumentOutOfRangeException e) {
+					Debug.LogWarning (e.Message);
+				} finally {
+					float[] floats = heights.ToArray ();
+					Array.Resize (ref floats, prop.arraySize);
+					heights = floats.ToList ();
+				}
+
+				return height;
+			};
+
+			list.onAddDropdownCallback = (rect, li) => {
+				var menu = new GenericMenu ();
+				menu.AddItem (new GUIContent ("Add Element"), false, () => {
+					serializedObject.Update ();
+					li.serializedProperty.arraySize++;
+					serializedObject.ApplyModifiedProperties ();
+				});
+
+				menu.ShowAsContext ();
+
+				float[] floats = heights.ToArray ();
+				Array.Resize (ref floats, prop.arraySize);
+				heights = floats.ToList ();
+			};
+ 
+			return list;
+
+		}
+
+	}
+}
